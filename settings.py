0831acb6f59d7d7039fa530d113ddb0e94098a26
Viktor Bochagov <vitekbochagov@mail.ru>
2025-02-16 14:57:48 +0500

    readme

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-EXCEL_PATH = BASE_DIR.joinpath('data/operations.xls')
+EXCEL_PATH = BASE_DIR.joinpath('data/operations.xlsx')
 JSON_PATH = BASE_DIR.joinpath('user_settings.json')
 LOG_PATH = BASE_DIR.joinpath('logs/app.log')
 REPORTS_PATH = BASE_DIR.joinpath('reports.json')
